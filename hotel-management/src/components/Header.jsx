ce8a6fb8af5dd62681560b95e7cb1e65bb29e9b7
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-05-23 23:28:20 +0530

    Create Header.jsx

---
+++ hotel-management/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-blue-900 text-white p-4">
+      <h1 className="text-xl font-bold">Sri Lanka Hotel Management</h1>
+    </header>
+  );
+}
