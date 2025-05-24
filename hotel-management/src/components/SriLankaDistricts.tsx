27956c25f50b2431df6658983f2aca041346567f
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-05-24 23:32:47 +0530

    Create SriLankaDistricts.tsx

---
+++ hotel-management/src/components/SriLankaDistricts.tsx
@@ -0,0 +1,12 @@
+export const districts = [
+  "Colombo", "Gampaha", "Kalutara", "Kandy", "Matale", "Nuwara Eliya",
+  "Galle", "Matara", "Hambantota", "Jaffna", "Kilinochchi", "Mannar",
+  "Vavuniya", "Mullaitivu", "Batticaloa", "Ampara", "Trincomalee",
+  "Kurunegala", "Puttalam", "Anuradhapura", "Polonnaruwa", "Badulla",
+  "Monaragala", "Ratnapura", "Kegalle"
+];
+
+export const provinces = [
+  "Western", "Central", "Southern", "Northern", "Eastern",
+  "North Western", "North Central", "Uva", "Sabaragamuwa"
+];
